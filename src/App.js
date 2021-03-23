1b745a936929a132294772ef6207fe8fc2b9ae71
Vehikl Team <go@vehikl.com>
2021-03-23 16:30:41 -0400

    Output a list of songs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
+    const [songs, setSongs] = useState([]);
+
     useEffect(async () => {
         const response = await fetch('http://localhost:3001')
         const payload = await response.json();
-        console.log(payload);
+        console.log(payload.songs)
+        setSongs(payload.songs);
     }, [])
 
     return (
         <>
-            <p>This is our app</p>
+            {
+                songs.map(song => {
+                    return (
+                        <div key={song.rank}>
+                            <p>{song.title}</p>
+                            <p>{song.artist}</p>
+                            <img src={song.cover} /><br></br>
+                        </div>
+                    );
+                })
+            }
         </>
     );
 }
